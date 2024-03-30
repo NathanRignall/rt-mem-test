2c4fefb9d6d4b78af71cdc6b6883958fa570fcf2
Nathan Rignall <rignallnathan@gmail.com>
2024-03-30 18:04:48 +0000

    Format code and add comments

--- src/bin/child.rs
+++ src/bin/child.rs
@@ -5,49 +5,64 @@ fn main() {
     println!("Hello, child!");
 
     // open shared memory
-    let send_shmem = ShmemConf::new().size(4096).flink("r_event_mapping").open().unwrap();
-    let recv_shmem = ShmemConf::new().size(4096).flink("s_event_mapping").open().unwrap();
-
+    let send_shmem = ShmemConf::new()
+        .size(4096)
+        .flink("r_event_mapping")
+        .open()
+        .unwrap();
+    let recv_shmem = ShmemConf::new()
+        .size(4096)
+        .flink("s_event_mapping")
+        .open()
+        .unwrap();
 
     // create event in shared memory
-    let (send_event, _) = unsafe { raw_sync::events::Event::from_existing(send_shmem.as_ptr()).unwrap() };
-    let (recv_event, _) = unsafe { raw_sync::events::Event::from_existing(recv_shmem.as_ptr()).unwrap() };
+    let (send_event, _) =
+        unsafe { raw_sync::events::Event::from_existing(send_shmem.as_ptr()).unwrap() };
+    let (recv_event, _) =
+        unsafe { raw_sync::events::Event::from_existing(recv_shmem.as_ptr()).unwrap() };
 
+    // create vector to store timestamps
     let mut times = Vec::new();
 
     // sleep for 100ms to allow the parent to set up
     std::thread::sleep(std::time::Duration::from_millis(1000));
 
     // acknowledge component init
-    send_event.set(raw_sync::events::EventState::Signaled).unwrap();
+    send_event
+        .set(raw_sync::events::EventState::Signaled)
+        .unwrap();
 
     println!("Child ready to receive");
-    
+
     let mut i = 0;
     loop {
         // wait for the parent to signal
         recv_event.wait(raw_sync::Timeout::Infinite).unwrap();
 
-        i+=1;
+        i += 1;
 
+        // store the timestamp
         let timestamp = std::time::SystemTime::now()
-                .duration_since(std::time::UNIX_EPOCH)
-                .unwrap()
-                .as_micros() as u64;
+            .duration_since(std::time::UNIX_EPOCH)
+            .unwrap()
+            .as_micros() as u64;
         times.push(timestamp);
 
         // signal the parent
-        send_event.set(raw_sync::events::EventState::Signaled).unwrap();
+        send_event
+            .set(raw_sync::events::EventState::Signaled)
+            .unwrap();
 
-        // finish after 10,000 iterations
+        // finish after 50,000 iterations
         if i == 50000 {
             break;
         }
-
     }
 
     println!("Goodbye, child! (Write)");
 
+    // write the timestamps to a file
     let mut writer = csv::Writer::from_path("times-child.csv").unwrap();
     for (i, timestamp) in times.iter().enumerate() {
         writer
--- src/bin/parent.rs
+++ src/bin/parent.rs
@@ -1,4 +1,4 @@
-use std::{process::{Command, Stdio}};
+use std::process::{Command, Stdio};
 
 use libc::cpu_set_t;
 use raw_sync::events::EventInit;
@@ -22,9 +22,7 @@ fn main() {
         let ret = libc::sched_setscheduler(
             0,
             libc::SCHED_FIFO,
-            &libc::sched_param {
-                sched_priority: 99,
-            },
+            &libc::sched_param { sched_priority: 99 },
         );
         if ret != 0 {
             println!("Failed to set scheduler");
@@ -32,17 +30,27 @@ fn main() {
     }
 
     // open shared memory
-    let send_shmem = ShmemConf::new().size(4096).flink("s_event_mapping").create().unwrap();
-    let recv_shmem = ShmemConf::new().size(4096).flink("r_event_mapping").create().unwrap();
+    let send_shmem = ShmemConf::new()
+        .size(4096)
+        .flink("s_event_mapping")
+        .create()
+        .unwrap();
+    let recv_shmem = ShmemConf::new()
+        .size(4096)
+        .flink("r_event_mapping")
+        .create()
+        .unwrap();
 
     // create event in shared memory
-    let (send_event, _) = unsafe { raw_sync::events::Event::new(send_shmem.as_ptr(), true).unwrap() };
-    let (recv_event, _) = unsafe { raw_sync::events::Event::new(recv_shmem.as_ptr(), true).unwrap() };
-        
+    let (send_event, _) =
+        unsafe { raw_sync::events::Event::new(send_shmem.as_ptr(), true).unwrap() };
+    let (recv_event, _) =
+        unsafe { raw_sync::events::Event::new(recv_shmem.as_ptr(), true).unwrap() };
+
     // spawn the child process
     let binary_path = format!("target/release/child");
     let mut command = Command::new(binary_path);
-    
+
     // redirect the child's stderr to the parent's stderr
     let child = command
         .stdout(Stdio::inherit())
@@ -54,7 +62,11 @@ fn main() {
     let mut cpu_set: cpu_set_t = unsafe { std::mem::zeroed() };
     unsafe {
         libc::CPU_SET(3, &mut cpu_set);
-        let ret = libc::sched_setaffinity(child.id() as libc::pid_t, std::mem::size_of_val(&cpu_set), &cpu_set);
+        let ret = libc::sched_setaffinity(
+            child.id() as libc::pid_t,
+            std::mem::size_of_val(&cpu_set),
+            &cpu_set,
+        );
         if ret != 0 {
             panic!("Failed to set affinity");
         }
@@ -65,9 +77,7 @@ fn main() {
         let ret = libc::sched_setscheduler(
             child.id() as libc::pid_t,
             libc::SCHED_FIFO,
-            &libc::sched_param {
-                sched_priority: 99,
-            },
+            &libc::sched_param { sched_priority: 99 },
         );
         if ret != 0 {
             println!("Failed to set scheduler");
@@ -76,10 +86,13 @@ fn main() {
 
     // wait for the component to be ready
     recv_event.wait(raw_sync::Timeout::Infinite).unwrap();
+
     println!("Child is ready");
 
+    // create vector to store timestamps
     let mut times = Vec::new();
 
+    // store loop timing information
     let mut last_time;
     let mut last_sleep = std::time::Duration::from_micros(0);
     let mut last_duration = std::time::Duration::from_micros(0);
@@ -88,27 +101,39 @@ fn main() {
 
     println!("Parent ready to send");
 
+    // now start looping to test the response time
     let mut i = 0;
     loop {
         last_time = std::time::Instant::now();
 
+        // store the timestamp
         let timestamp = std::time::SystemTime::now()
             .duration_since(std::time::UNIX_EPOCH)
             .unwrap()
             .as_micros() as u64;
-        times.push((timestamp, last_sleep.as_micros() as u64, last_duration.as_micros() as u64, overruns));
+        times.push((
+            timestamp,
+            last_sleep.as_micros() as u64,
+            last_duration.as_micros() as u64,
+            overruns,
+        ));
 
-        i+=1;
-        
-        send_event.set(raw_sync::events::EventState::Signaled).unwrap();
+        i += 1;
 
+        // signal the child
+        send_event
+            .set(raw_sync::events::EventState::Signaled)
+            .unwrap();
+
+        // wait for the child to signal back
         recv_event.wait(raw_sync::Timeout::Infinite).unwrap();
 
-        // finish after 10,000 iterations
+        // finish after 50,000 iterations
         if i == 50000 {
             break;
         }
 
+        // update loop timing information
         let now = std::time::Instant::now();
         let duration = now.duration_since(last_time);
         let mut sleep = std::time::Duration::from_micros(0);
@@ -131,6 +156,7 @@ fn main() {
 
     println!("Goodbye, parent! (Write)");
 
+    // write the timestamps to a CSV file
     let mut writer = csv::Writer::from_path("times-parent.csv").unwrap();
     for (i, (timestamp, sleep, duration, overruns)) in times.iter().enumerate() {
         writer
